b3d95969c50e1a20f1861f937010c2e3d7907bf9
yaho-ma <dev76913@gmail.com>
2025-03-10 02:27:02 +0200

    users andpoint on render.com

--- config.py
+++ config.py
@@ -3,7 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
